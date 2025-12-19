d739fc8c79d00964f3779a07f1d3ca38698e7396
finvesco <oyugal@finvesco.com>
2025-12-19 21:23:03 +0530

    Check the apis

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // This allows you to access images via http://localhost:PORT/uploads/filename.jpg
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use('/api/nyaya/', nyayaRoutes);
+app.use('/api/nyaya', nyayaRoutes);
 
 app.get('/nyaya', (req, res) => { res.send("Nyaya-Lite Backend Running") });
 
